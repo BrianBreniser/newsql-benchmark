76cdb5f74d91b8519779d46f9a48ee7a51362243
Brian Breniser <git@breniser.me>
2023-10-30 08:09:48 -0700

    bump

--- openshift_benchmarks/loop_get_logs_lastline.sh
+++ openshift_benchmarks/loop_get_logs_lastline.sh
@@ -2,6 +2,8 @@
 
 checkAllStarted="true"
 notify-send "Starting log loop"
+start_time=$(date +%s)
+gatherLatencyMetrics="true"
 
 while true; do
     pods=$(oc get pods | rg -i benchmark | awk '{print $1}')
@@ -26,6 +28,8 @@ while true; do
 
     done
 
+    # If all pods have started, then wait 2 minutes for pods to warm up
+    # Then collect metrics from fdbcli
     if [[ "$allStarted" == "true" && "$checkAllStarted" == "true" ]]; then
         notify-send "All pods started"
         checkAllStarted="false"
@@ -34,15 +38,17 @@ while true; do
         sleep 120
         notify-send "Collecting metrics from fdbcli"
         echo "Collecting metrics from fdbcli"
-        fdb exec -c fdb-cluster-1 -- fdbcli --exec "status details" | rg Redundancy >> results.txt
-        echo "" >> results.txt
+        #fdb exec -c fdb-cluster-1 -- fdbcli --exec "status details" | rg Redundancy >> results.txt
+        #echo "" >> results.txt
         # Had some broken pipe problems without using a tmp file.
         fdb exec -c fdb-cluster-1 -- fdbcli --exec "status details" > tmp.txt
-        grep cpu tmp.txt | head >> results.txt
+        head -50 tmp.txt >> results.txt
         rm tmp.txt
         echo "" >> results.txt
     fi
 
+
+    # If all pods are done, then break out of the loop
     if [[ "$allDone" == "true" ]]; then
         echo "All pods finished, now collecting metrics into results.txt"
         notify-send "All pods finished, now collecting metrics into results.txt"
@@ -50,9 +56,28 @@ while true; do
         break
     fi
 
+    # Needs to run last, because of the 'continue' statement that skips the rest of the loop
+    # If 5 minutes have passed, then collect latency metrics
+    current_time=$(date +%s)
+    if (( current_time > start_time + 150 )); then
+        if [[ "$gatherLatencyMetrics" == "true" ]]; then
+            echo "5 minutes have passed, saving latency probe and grv_latency metrics into results.txt"
+            notify-send "5 minutes have passed, saving latency probe and grv_latency metrics into results.txt"
+
+            echo "latency probes after 5 minutes of running" >> results.txt
+            echo "" >> results.txt
+            fdb exec -c fdb-cluster-1 -- fdbcli --exec "status json" > tmp.txt
+            cat tmp.txt | jq '{latency_probe: .cluster.latency_probe}' >> results.txt
+            fdb exec -c fdb-cluster-1 -- fdbcli --exec "status json" > tmp.txt
+            cat tmp.txt | jq '.cluster.processes[].roles[].grv_latency_statistics' | grep -v '^null$' >> results.txt
+
+            gatherLatencyMetrics="false"
+        fi
+    fi
+
     sleep 30
 done
 
-echo "Completely finished"
-notify-send "Completely finished"
+echo "Run completely finished"
+notify-send "Run completely finished"
 
--- openshift_benchmarks/reset_ycsb.sh
+++ openshift_benchmarks/reset_ycsb.sh
@@ -10,6 +10,10 @@ notify-send "Waiting for pods to terminate"
 
 notify-send "Templating $1 applied"
 
+notify-send "Gathering ycsb and fdb output"
+echo "Gathering ycsb and fdb output"
+./get_ycsb_fdb_setup.sh # gets ycsb and fdb output and puts it in results.txt automatically
+
 # Wait for the old pods to be deleted
 non_running_pods=$(oc get pods | rg -i terminating | wc -l)
 
