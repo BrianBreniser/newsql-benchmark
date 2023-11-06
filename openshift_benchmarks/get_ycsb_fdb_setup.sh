1b026f876c00802c333e8eb25251dbdd5b579bbb
Brian Breniser <git@breniser.me>
2023-11-06 09:55:54 -0800

    bump

--- openshift_benchmarks/get_ycsb_fdb_setup.sh
+++ openshift_benchmarks/get_ycsb_fdb_setup.sh
@@ -10,14 +10,14 @@ echo "$ycsbsetup" >> results.txt
 echo "" >> results.txt
 
 # Get the FDB setup
-proccesscounts=$(oc get fdb fdb-cluster-1 -o yaml | yq e '.spec.processCounts')
-databaseconfig=$(oc get fdb fdb-cluster-1 -o yaml | yq e '.spec.databaseConfiguration')
+#proccesscounts=$(oc get fdb fdb-cluster-1 -o yaml | yq e '.spec.processCounts')
+#databaseconfig=$(oc get fdb fdb-cluster-1 -o yaml | yq e '.spec.databaseConfiguration')
 
 echo "Database Config:" >> results.txt
-echo "$proccesscounts" >> results.txt
-echo "" >> results.txt
+#echo "$proccesscounts" >> results.txt
+#echo "" >> results.txt
 
 echo "Process Counts:" >> results.txt
-echo "$databaseconfig" >> results.txt
-echo "" >> results.txt
+#echo "$databaseconfig" >> results.txt
+#echo "" >> results.txt
 
